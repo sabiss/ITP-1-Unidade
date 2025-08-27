08c6730699fe856a6ce793ba2ea39d4cf747b1c5
sabiss <sabrinabezsilva@gmail.com>
2025-08-27 11:17:22 -0300

    código terminal

--- 1-unidade/corrida.cpp
+++ 1-unidade/corrida.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    pair<double,int> tempos[3] = {};
+    pair<double, int> tempos[3] = {};
+
     for(int i = 0; i < 3; i++){
         cin>>tempos[i].first;
         tempos[i].second=i+1;
+    }
+
+    sort(tempos, tempos+3);
+
+    cout << fixed<<setprecision(1);
+    for(int i = 0; i < 3; i++){
+        cout<< i+1<<"o lugar: Competidor " << tempos[i].second<< " com " <<tempos[i].first<< " segundos"<<endl;
     }
 }
